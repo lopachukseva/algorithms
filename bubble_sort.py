a2f803a3f1af3da83b9af7803e279045aa9cb639
Seva Lopachuk <lopachuk.seva@gmail.com>
2023-03-09 21:28:33 +0100

    add bubble sort

---
+++ bubble_sort.py
@@ -0,0 +1,14 @@
+def bubble_sort(lst):
+    iterations_count = len(lst) + 1
+
+    for _ in range(1, iterations_count):
+        for j in range(1, len(lst)):
+            if lst[j - 1] > lst[j]:
+                lst[j], lst[j - 1] = lst[j - 1], lst[j]
+
+
+nums_list = [51, 1, 3, -6, 5, 10, 4, 0, 19]
+
+bubble_sort(nums_list)
+
+print(nums_list)
